6bc050719e448f12ab4f966da77b152e918028cc
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-23 17:55:46 -0500

    Instalación y configuración de Redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppRouter } from "./router/AppRouter.jsx";
 
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   </StrictMode>
 );
