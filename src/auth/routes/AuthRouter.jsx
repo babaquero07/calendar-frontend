bf411581e8493383f6e82c08c67ea57b8eb126ec
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-23 14:06:01 -0500

    LoginScreen y Navbar

--- src/auth/routes/AuthRouter.jsx
+++ src/auth/routes/AuthRouter.jsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
 
+const status = "authenticated";
 export const AuthRouter = () => {
   // const {status} = useSelector(state => state.auth)
 
-  // if(status === 'authenticated') return <Navigate to="/" />
+  if (status === "authenticated") return <Navigate to="/" />;
 
   return <Outlet />;
 };
