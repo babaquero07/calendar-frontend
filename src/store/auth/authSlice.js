6bb567c781ed97978d6776497c1cbae28454dff3
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-25 16:08:48 -0500

    Add authSlice to store.js

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: "checking", // 'checking' | 'authenticated' | 'not-authenticated'
+  user: {},
+  errorMessage: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    onChecking: (state) => {
+      state.status = "checking";
+      state.user = {};
+      state.errorMessage = null;
+    },
+    onLogin: (state, { payload }) => {
+      state.status = "authenticated";
+      state.user = payload;
+      state.errorMessage = null;
+    },
+  },
+});
+
+export const { onChecking, onLogin } = authSlice.actions;
